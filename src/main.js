77c6823a8606c286679f5a14bddd27c7df2ff35f
jeroentuk <tukky93@gmail.com>
2017-06-23 14:33:43 +0200

    Main js update voor playerlist

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import _ from 'underscore';
 import {Events} from 'backbone';
 import Matches from './collections/Matches';
-//import Players from './collections/Players';
+import Players from './collections/Players';
 import TeamLinks from './views/TeamLinks';
-//import TeamPlayerLinks from './views/TeamPlayerLinks';
+import TeamPlayerLinks from './views/TeamPlayerLinks';
 import TeamMatches from './views/TeamMatches';
-//import TeamPlayers from './views/TeamPlayers';
+import TeamPlayers from './views/TeamPlayers';
 
 
 (function ()
@@ -15,7 +15,7 @@ import TeamMatches from './views/TeamMatches';
         window.App = {};
         App.events = _.clone(Events);
     };
-
+    
     /**
      * Run after dom is ready
      */
@@ -26,6 +26,9 @@ import TeamMatches from './views/TeamMatches';
         let matchesCollection = new Matches();
         new TeamLinks({el: "#team-links"});
         new TeamMatches({el: "#team-matches", collection: matchesCollection});
+        let playersCollection = new Players();
+        new TeamPlayerLinks({el: "#team-players"});
+        new TeamPlayers({el: "#team-playerslist", collection: playersCollection});
 
 
         Backbone.history.start({pushState: true, root: '/FED01/voetbal_jeroen/'});
